f55bdd1fe4df4b4742208f85b8aeca79cdf1767c
剑雪无名 <cloud@shopes.cn>
2023-04-15 22:13:57 +0800

    update close #38 4.15.22[11.57] 第29.6周 清明/谷雨8-5

--- public/git.php
+++ public/git.php
@@ -8,7 +8,7 @@ ignore_user_abort(true);
 set_time_limit(0);
 
 echo shell_exec("git fetch --all 2>&1") . PHP_EOL;
-echo shell_exec("git reset --hard origin/master 2>&1") . PHP_EOL;
+echo shell_exec("git reset --hard origin/main 2>&1") . PHP_EOL;
 
 echo "finish" . PHP_EOL;
 
